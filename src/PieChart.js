5800a89b8e6e3b2f91671ccdc10d2dbb59811f88
MiaoMiaoMiao3 <uyen.a.tran@gmail.com>
2021-05-11 20:08:27 -0700

    NPM install react-google-charts

--- src/PieChart.js
+++ src/PieChart.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
-
+import Chart from "react-google-charts";
 
 class PieChart extends Component{
     render(){
         return(
-            <div>
-                From the pie chart
-            </div>
+            <Chart
+                width={'500px'}
+                height={'300px'}
+                chartType="PieChart"
+                loader={<div>Loading Chart</div>}
+                data={[
+                    ['Task', 'Hours per Day'],
+                    ['Work', 11],
+                    ['Eat', 2],
+                    ['Commute', 2],
+                    ['Watch TV', 2],
+                    ['Sleep', 7],
+                ]}
+                options={{
+                    title: 'My Daily Activities',
+                }}
+                rootProps={{ 'data-testid': '1' }}
+                />
         )
     }
 }
