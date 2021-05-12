62d43cd310ea5e7ed1be666f71c082a31c1c1301
MiaoMiaoMiao3 <uyen.a.tran@gmail.com>
2021-05-11 21:37:03 -0700

    Set up AJAX requests. Add func to calculate property sales/agent.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
+import React, {Component} from 'react'
 import './App.css';
-import './PieChart';
+import axios from "axios";
 import PieChart from './PieChart';
 
-function App() {
-  return (
-    <div className="App">
-        <PieChart/>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      agents: [],
+      property:[]
+    }
+    this.calcTotal = this.calcTotal.bind(this);
+  }
+
+  async componentDidMount() {
+    const API_AGENT_URL = `http://localhost:3100/agent`;
+    const API_PROPERTY_URL = `http://localhost:3100/property-type`;
+    let responseAgent = await axios.get(API_AGENT_URL);
+    let responseProperty = await axios.get(API_PROPERTY_URL);
+    this.setState({agents: responseAgent.data, property: responseProperty.data});
+  }
+
+  calcTotal (targetAgent){
+    let totalCount = {};
+    let property = this.state.property
+    let agents = this.state.agents
+
+    for(let i =0; i<agents.length; i++){
+        if(agents[i] === targetAgent){
+
+            totalCount[property[i]]? totalCount[property[i]]++ : totalCount[property[i]]=1;
+            
+            }
+        }
+        return totalCount
+    }
+
+
+    render(){
+      console.log(this.calcTotal("steve"));
+      return(
+            <PieChart agents = {this.state.agents} property = {this.state.property}/>
+        )
+    }
 }
 
-export default App;
+export default App
